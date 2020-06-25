a71b13c2d5c179abe7c3f84510156a36e55486a9
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-06-26 04:22:08 +0900

    新規投稿機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
 
-  def
+  def new
     @user = User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age)
   end
 
 end
