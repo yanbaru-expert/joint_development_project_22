b4ae5be5115d67ee1e45d6484e82bbe05ef99993
K.Fujisawa <fujisawakazuya@MacBook-Air.local>
2020-06-25 14:04:49 +0900

    add 削除機能
    destroy.html.erb作成済

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -24,6 +24,11 @@ class MessagesController < ApplicationController
     @message = Message.find(params[:id])
   end
 
+  def destroy
+    message = Message.find(params[:id])
+    message.destroy
+  end
+
   private
   def message_params
     params.require(:message).permit(:title, :content)
